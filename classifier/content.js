f9f692e379e79a60de6bbd15e53e0697a626a26d
Jaiganesh-MK <jaiganeshshs@gmail.com>
2020-04-21 14:59:59 +0530

    background and content scripts

---
+++ classifier/content.js
@@ -0,0 +1,16 @@
+// Sends the text contents of the webpage being viewed to the background script
+var port = chrome.runtime.connect({name: "port"});
+port.postMessage({contents:document.body.innerText});
+port.onMessage.addListener(function(msg){
+    // data containing offensive words is received from background script
+    received_data = msg.result;
+    const len = received_data.length;
+    for( var i = 0; i<len; i++){
+        for(var j=0;j<document.getElementsByTagName("body").length;j++){
+            var to_hide = new RegExp(received_data[i],"gi");
+            // replacing the offensive words with ****
+            document.body.innerHTML = document.body.innerHTML.replace(to_hide,'*****');
+        }
+    }
+});
+
