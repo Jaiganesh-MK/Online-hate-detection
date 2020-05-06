c525bf5f5600f43ada53d01b96f0eb89d4ec69c9
Jaiganesh-MK <jaiganeshshs@gmail.com>
2020-05-07 00:42:48 +0530

    loaded classifier once

--- chrome extension/app.py
+++ chrome extension/app.py
@@ -12,6 +12,14 @@ from sklearn.ensemble import RandomForestClassifier
 
 app = Flask(__name__)
 
+df = pd.read_csv("labeled_data.csv")
+vectorizer = CountVectorizer()
+vectorizer.fit_transform(df['tweet'].values.astype('U'))
+# load the trained model
+rf = joblib.load('NM_classifier.pkl')
+
+def predict(str):
+    return rf.predict(vectorizer.transform([str]))
 
 @app.route("/")
 def index():
@@ -19,17 +27,21 @@ def index():
 
 @app.route('/_get_data/', methods=['POST'])
 def _get_data():
-    data = request.form.get("data")
-    df = pd.read_csv("labeled_data.csv")
-    vectorizer = CountVectorizer()
-    vectorizer.fit_transform(df['tweet'].values.astype('U'))
-    # load the trained model
-    rf = joblib.load('NM_classifier.pkl')
-    a = str(data).split()
-    hate = ['dafuq']
-    for i in range(0,len(a)):
-        if(rf.predict(vectorizer.transform([a[i]]))==1):
-            hate.append(a[i])
+    data = {}
+    data["success"] = False
+    if request.method == "POST":
+        data["hate"] = []
+        content = request.form.get("data")
+        a = str(content).split()
+        hate = []
+        for i in range(0,len(a)):
+            if(predict(a[i])==1):
+                data['hate'].append(a[i])
+        data["success"] = True
+        
    
-    return jsonify({"rate":hate})
+    return jsonify(data)
+
+if __name__ == '__main__':
+    app.run(debug=True)
 
--- chrome extension/background.js
+++ chrome extension/background.js
@@ -1,18 +1,26 @@
 chrome.runtime.onConnect.addListener(function(port){
+    
     console.assert(port.name == "port");
     // receives the message from content.js
+    
     port.onMessage.addListener(function(msg){
+        
         var data = msg.contents;
         var xhttp = new XMLHttpRequest();
         // created ajax request to the flask server 
         xhttp.open('POST','http://127.0.0.1:5000//_get_data/');
 
         xhttp.onload = () => {
+
             const r = JSON.parse(xhttp.responseText);
             // received data containing offensive words is sent back to content script
-            port.postMessage({result:r.rate});
-            console.log("here");
+            if(r.success == true){
+                port.postMessage({result:r.hate});
+                console.log("hate words received");
+            }
+            
         };
+        
         // sends the data to the flask server
         const mydata = new FormData();
         mydata.append('data', data);
